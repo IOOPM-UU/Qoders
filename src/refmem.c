e6573a6a3fa61ee7c30a775415553b168e95e8ac
Hector Bergman <hector.bergman.4575@student.uu.se>
2023-12-19 11:08:40 +0100

    edjkGKS

--- src/refmem.c
+++ src/refmem.c
@@ -4,138 +4,132 @@
 #include <string.h>
 #include <stdbool.h>
 
-<<<<<<< Updated upstream obj *allocate(size_t bytes, function1_t destructor)
+size_t cascade_limit = 100;
+
+hash_table_t ht;
+
+obj *allocate(size_t bytes, function1_t destructor)
 {
-    == == == =
-                 size_t cascade_limit = 100;
 
-    hash_table_t ht;
+    obj *new_object = (obj *)malloc(sizeof(meta_data_t) + bytes);
 
-    obj *allocate(size_t bytes, function1_t destructor)
+    if (new_object != NULL)
     {
->>>>>>> Stashed changes
+        meta_data_t *meta_data = (meta_data_t *)new_object;
 
-        obj *new_object = (obj *)malloc(sizeof(meta_data_t) + bytes);
-
-        if (new_object != NULL)
-        {
-            meta_data_t *meta_data = (meta_data_t *)new_object;
-
-            meta_data->next = NULL;
-            meta_data->reference_counter = 0;
-            meta_data->destructor = destructor;
-            meta_data->garbage = true;
-        }
-
-        return new_object;
+        meta_data->next = NULL;
+        meta_data->reference_counter = 0;
+        meta_data->destructor = destructor;
+        meta_data->garbage = true;
     }
 
-    // we could make a hashtable that's dynamic
-    // as we get closer to the threshold of our HT, we'll the double the amount of buckets
-    // then resize all of the entries that we previously added
+    return new_object;
+}
 
-    obj *allocate_array(size_t elements, size_t elem_size, function1_t destructor)
+// we could make a hashtable that's dynamic
+// as we get closer to the threshold of our HT, we'll the double the amount of buckets
+// then resize all of the entries that we previously added
+
+obj *allocate_array(size_t elements, size_t elem_size, function1_t destructor)
+{
+
+    obj *new_object = (obj *)calloc(elements, elem_size);
+
+    if (new_object != NULL)
     {
+        meta_data_t *meta_data = (meta_data_t *)new_object;
 
-        obj *new_object = (obj *)calloc(elements, elem_size);
-
-        if (new_object != NULL)
-        {
-            meta_data_t *meta_data = (meta_data_t *)new_object;
-
-            meta_data->next = NULL;
-            meta_data->adress = &new_object;
-            meta_data->reference_counter = 0;
-            meta_data->destructor = destructor;
-            meta_data->garbage = true;
-        }
-
-        return new_object;
+        meta_data->next = NULL;
+        meta_data->adress = &new_object;
+        meta_data->reference_counter = 0;
+        meta_data->destructor = destructor;
+        meta_data->garbage = true;
     }
 
-    void retain(obj * c)
+    return new_object;
+}
+
+void retain(obj *c)
+{
+    meta_data_t *m = (meta_data_t *)c;
+    m->reference_counter++;
+    m->garbage = false;
+}
+
+void release(obj *c)
+{
+
+    meta_data_t *meta_data = (meta_data_t *)c;
+
+    if (c != NULL && meta_data->reference_counter >= 1)
     {
         meta_data_t *m = (meta_data_t *)c;
-        m->reference_counter++;
-        m->garbage = false;
+        m->reference_counter--;
     }
 
-    void release(obj * c)
+    if (meta_data->reference_counter == 0)
     {
-
-        meta_data_t *meta_data = (meta_data_t *)c;
-
-        if (c != NULL && meta_data->reference_counter >= 1)
-        {
-            meta_data_t *m = (meta_data_t *)c;
-            m->reference_counter--;
-        }
-
-        if (meta_data->reference_counter == 0)
-        {
-            meta_data->garbage = true;
-        }
-
-        // else {
-        //      temp_deallocate(&c);
-        //  }
-
-        // CLEAN-UP checks if destructor exists or not
+        meta_data->garbage = true;
     }
 
-    size_t rc(obj * c)
+    // else {
+    //      temp_deallocate(&c);
+    //  }
+
+    // CLEAN-UP checks if destructor exists or not
+}
+
+size_t rc(obj *c)
+{
+    meta_data_t *meta_data = (meta_data_t *)c;
+    // meta_data->reference_counter++;
+    return meta_data->reference_counter;
+}
+
+void deallocate(obj *c)
+{
+    meta_data_t *m = (meta_data_t *)c;
+
+    if (rc(c) == 0)
     {
-        meta_data_t *meta_data = (meta_data_t *)c;
-        // meta_data->reference_counter++;
-        return meta_data->reference_counter;
+        m->destructor(c);
     }
 
-    void deallocate(obj * c)
+    free(m);
+}
+void temp_deallocate(obj **object)
+{
+    free(*object);  // Free the object
+    *object = NULL; // Destroy the pointer to the object
+}
+
+void cleanup()
+{
+    for (int i = 0; i < No_Buckets; i++)
     {
-        meta_data_t *m = (meta_data_t *)c;
-
-        if (rc(c) == 0)
+        if (ht->buckets[i].next != NULL)
         {
-            m->destructor(c);
-        }
+            entry_t *current = ht->buckets[i].next;
+            entry_t *next_entry = current->next;
 
-<<<<<<< Updated upstream
-        free(m);
-        == == == =
-                     void temp_deallocate(obj * *object)
-        {
-            free(*object);  // Free the object
-            *object = NULL; // Destroy the pointer to the object
-        }
-
-        void cleanup()
-        {
-            for (int i = 0; i < No_Buckets; i++)
+            while (next_entry != NULL)
             {
-                if (ht->buckets[i].next != NULL)
-                {
-                    entry_t *current = ht->buckets[i].next;
-                    entry_t *next_entry = current->next;
-
-                    while (next_entry != NULL)
-                    {
-                        deallocate(current);
-                        current = next_entry;
-                        next_entry = current->next;
-                    }
-                    deallocate(current);
-                }
+                deallocate(current);
+                current = next_entry;
+                next_entry = current->next;
             }
+            deallocate(current);
         }
+    }
+}
 
-        void set_cascade_limit(size_t lim)
-        {
-            cascade_limit = lim;
->>>>>>> Stashed changes
-        }
+void set_cascade_limit(size_t lim)
+{
+    cascade_limit = lim;
+}
 
-        void temp_deallocate(obj * *object)
-        {
-            free(*object);  // Free the object
-            *object = NULL; // Destroy the pointer to the object
-        }
+void temp_deallocate(obj **object)
+{
+    free(*object);  // Free the object
+    *object = NULL; // Destroy the pointer to the object
+}
